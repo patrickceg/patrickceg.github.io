e8a3e4ddf6de0f72d6df63ea3ded14fcba340057
Patrick S <patrickceg@outlook.com>
2018-06-03 14:09:59 -0400

    Add default Jekyll template

--- _debug.ps1
+++ _debug.ps1
@@ -1,4 +1,6 @@
 # This is a reference script I use for my templates to be able to build the site using
 # Docker for Windows
 
-docker run --rm --volume="$PWD:/srv/jekyll" -it jekyll/builder jekyll build
+docker run -d -p 8043:8043 -v "${PWD}/_site:/srv/http" --name goStatic pierrezemb/gostatic
+docker run --rm --volume="${PWD}:/srv/jekyll" -it jekyll/builder jekyll build --watch
+docker rm -f goStatic
