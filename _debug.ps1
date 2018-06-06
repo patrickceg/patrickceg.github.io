73e693aa622928edb392405bbb297cff1a648a47
Patrick S <patrickceg@outlook.com>
2018-06-05 21:21:03 -0400

    Remove RSS plug from home page

--- _debug.ps1
+++
@@ -1,6 +0,0 @@
-# This is a reference script I use for my templates to be able to build the site using
-# Docker for Windows
-
-docker run -d -p 8043:8043 -v "${PWD}/_site:/srv/http" --name goStatic pierrezemb/gostatic
-docker run --rm --volume="${PWD}:/srv/jekyll" -it jekyll/builder jekyll build --watch
-docker rm -f goStatic
