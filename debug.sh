06f8ef7c42c6ab0e9873cc286ec56a7d042ed959
Patrick S <patrickceg@outlook.com>
2019-02-15 15:26:25 -0500

    Add ruby version manager init to debug script

--- debug.sh
+++ debug.sh
@@ -1,5 +1,11 @@
 #! /bin/bash
 # For debugging
 
+# Enable the Ruby Version Manager environment
+RVM_SCRIPT_LOCAL=~/.rvm/scripts/rvm
+if [ -f $RVM_SCRIPT_LOCAL ]; then
+	source $RVM_SCRIPT_LOCAL
+fi
+
 bundle exec jekyll clean
 bundle exec jekyll serve --watch
